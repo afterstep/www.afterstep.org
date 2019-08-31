0fea162e0577c03648765f87957b64c4cb5d0442
Remco Rijnders <remco@webconquest.com>
2019-08-31 13:19:00 -0400

    Remove double headers from page

--- documentation/afterstep_faq.php
+++ documentation/afterstep_faq.php
@@ -1,9 +1,3 @@
-&nbsp;<? local_doc_url("documentation.php","Preface","",$srcunset,$subunset) ?>
- &nbsp;<? local_doc_url("visualdoc.php","Introduction","visualselect",$srcunset,$subunset) ?>
- &nbsp;<? local_doc_url("visualdoc.php","Topic index","index",$srcunset,$subunset) ?>
- &nbsp;<? local_doc_url("visualdoc.php","Glossary","Glossary",$srcunset,$subunset) ?>
- &nbsp;<? local_doc_url("visualdoc.php","F.A.Q.","faq",$srcunset,$subunset) ?>
- &nbsp;<? local_doc_url("visualdoc.php","Copyright","copyright",$srcunset,$subunset) ?>
  <hr>
 <br><table width=100%><tr><td width=50%><b><FONT face="Tahoma, Arial, Verdana, Helvetica" size="-1">AfterStep FAQ</FONT></b></td><td width=50%><FONT face="Tahoma, Arial, Verdana, Helvetica" size="-1">This document is an ever growing set of questions, statements, ideas and complaints about AfterStep version 2.0</FONT></td></tr></table><br><hr>
 <hr>
