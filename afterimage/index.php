0bac47581a6fabab4561a5b4abe6df23aa913a9c
Remco Rĳnders <remco@webconquest.com>
2020-11-13 11:18:50 +0100

    Minor spelling fixes libAfterImage

--- afterimage/index.php
+++ afterimage/index.php
@@ -3,13 +3,13 @@
 			<font size=3>libAfterImage</font>
 			<font size=2> is a generic image manipulation library.
 			It was initially implemented to address <A href="http://www.afterstep.org">AfterStep Window Manager</A>'s needs for
-			image handling, but it evolved into extremely powerfull and flexible software, suitable for virtually any project
+			image handling, but it evolved into extremely powerful and flexible software, suitable for virtually any project
 			that has needs for loading, manipulating, displaying images, as well as writing images in files. Most of the popular
 			image formats are supported using standard libraries, with <A href="http://www.gimp.org">XCF</A>, XPM, PPM/PNM,
 			BMP, ICO, TGA and GIF being supported internally.<p> 
-			PNG, JPEG and TIFF formats are supported via standard libraries. For convinience, 
-			source for libJPEG and libPNG is supplied with libAfterImage. <p>
-			Powerfull text rendering capabilities included, providing support for TrueType fonts using <A href="http://www.freetype.org">FreeType library</A>, and
+			PNG, JPEG and TIFF formats are supported via standard libraries. For convenience,
+			source for libJPEG and libPNG are supplied with libAfterImage. <p>
+			Powerful text rendering capabilities are included, providing support for TrueType fonts using <A href="http://www.freetype.org">FreeType library</A>, and
 			antialiasing of standard fonts from <A href="http://www.xfree86.org">X window system</A>. <p>
 			Primary design goals of libAfterImage are memory efficiency and <A href="ss_sub3.php">superior image quality</A>.<p>
 			Extensive <A href="documentation.php">documentation</A>, <A href="examples.php">examples</A>, and
